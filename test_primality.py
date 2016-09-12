10fe517f88fdfc3dc17ab2e37bd82f51668a7d14
Frank <franktheo1@yahoo.com>
2016-09-12 15:00:39 -0700

    first commit

---
+++ test_primality.py
@@ -0,0 +1,26 @@
+import unittest
+from primality import is_prime
+
+class PrimalityTestCase(unittest.TestCase):
+    """Tests for `primality.py`."""
+
+    def test_is_five_prime(self):
+         """Is five successfully determined to be prime?"""
+         self.assertTrue(is_prime(5))
+ 
+    def test_is_four_non_prime(self):
+         """Is four correctly determined not to be prime?"""
+         self.assertFalse(is_prime(4), msg='Four is not prime!')
+
+    def test_is_zero_not_prime(self):
+         """Is zero correctly determined not to be prime?"""
+         self.assertFalse(is_prime(0))
+
+    def test_negative_number(self):
+         """Is a negative number correctly determined not to be prime?"""
+         for index in range(-1, -10, -1):
+            self.assertFalse(is_prime(index))
+
+if __name__ == '__main__':
+    unittest.main()
+
